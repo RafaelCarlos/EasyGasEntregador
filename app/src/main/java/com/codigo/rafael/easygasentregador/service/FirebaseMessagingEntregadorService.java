ab970123954f004ccc3e79c099dd3a8dc06e3bee
Rafael Carlos <shanglangjunior@hotmail.com>
2017-10-31 16:13:16 -0300

    Integrando App com Firebase Messaging.

--- app/src/main/java/com/codigo/rafael/easygasentregador/service/FirebaseMessagingEntregadorService.java
+++ app/src/main/java/com/codigo/rafael/easygasentregador/service/FirebaseMessagingEntregadorService.java
@@ -1,6 +1,14 @@
 package com.codigo.rafael.easygasentregador.service;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.codigo.rafael.easygasentregador.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by Rafael Carlos Oliveira on 31/10/2017.
@@ -12,4 +20,23 @@ import com.google.firebase.messaging.FirebaseMessagingService;
  */
 public class FirebaseMessagingEntregadorService extends FirebaseMessagingService {
 
+    private static final int ID_NOTIFICACAO = 1;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setContentTitle("Notificação do Firebase")
+                        .setSound(defaultSoundUri)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentText(remoteMessage.getNotification().getBody());
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        mNotificationManager.notify(ID_NOTIFICACAO, mBuilder.build());
+    }
 }
